9f730251435d5d457b97788c109e7b19d8dcc704
Chae Hyo Ju <agleor@naver.com>
2023-04-27 13:45:14 +0900

    devserver 연결

--- study/webpack.config.js
+++ study/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
     name: 'wordrelay-setting',
     mode: 'development', //실서비스: production
     devtool: 'eval',
     resolve: {
-        extensions: ['js', '.jsx']
+        extensions: ['.js', '.jsx']
     },
     
     //입력
@@ -19,20 +20,32 @@ module.exports = {
             options: {
                 presets: [
                     ['@babel/preset-env', {
+                        debug: true,
                         targets: {
-                           browsers: ['> 1% in KR'] //브라우저 리스트
+                           browsers: ['> 1% in KR'],
                         }, 
-                    debug: true,
                     }],
                     '@babel/preset-react'
                 ],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             },
         }],
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
     //출력
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
-    }
+        filename: 'app.js',
+        publicPath: '/dist',
+    },
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    },
 }
