a92760acc2ca5b9ef8879774e8942dce00e9e2a4
Chae Hyo Ju <agleor@naver.com>
2023-05-08 18:01:24 +0900

    한 번 더 구현

--- 로또추첨기/Lotto.jsx
+++ 로또추첨기/Lotto.jsx
@@ -20,6 +20,59 @@ class Lotto extends Component{
         bonus: null,
         redo: false,
     };
+
+    timeouts =[];
+
+    runTimeouts = () =>{
+        console.log('runTimeout')
+        const {winNumbers} = this.state;
+        for(let i = 0; i<winNumbers.length-1; i++){
+            this.timeouts[i] = setTimeout(()=>{
+                this.setState((prevState) =>{
+                    return{
+                        winBalls: [...prevState.winBalls, winNumbers[i]]
+                    }
+                })
+            }, (i+1)*1000)
+        }
+        this.timeouts[6]=setTimeout(()=> {
+            this.setState({
+                bonus: winNumbers[6],
+                redo: true,
+            })
+        }, 7000)
+    }
+
+    componentDidMount() {
+        console.log('didMount')
+        this.runTimeouts();
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        console.log('didUpdate')
+        if(this.state.winBalls.length === 0) {
+            this.runTimeouts();
+        }
+    }
+
+    componentWillUnmount(){
+        console.log('unmount')
+        this.timeouts.forEach((v)=> {
+            clearTimeout(v);
+        })
+    }
+
+    onClickRedo =() => {
+        console.log('onclickRedo')
+        this.setState({
+            winNumbers: getWinNumbers(),
+            winBalls: [],
+            bonus: null,
+            redo: false,
+        })
+        this.timeouts = [];
+    }
+    
     render() {
         const { winBalls, bonus, redo} =this.state;
         return(
@@ -28,8 +81,9 @@ class Lotto extends Component{
                 <div id="결과창">
                     {winBalls.map((v)=> <Ball key={v} number={v} />)}
                 </div>
+                <div>보너스!</div>
                 {bonus && <Ball number={bonus} />}
-                <button onClick={redo?onClickRedo : () => {}}>한 번 더!</button>
+                {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
             </>
         )
     }
